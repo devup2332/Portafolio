5c2707e773749e78e64f4870937863ecd4b82ffa
Diego Raul <devup2332@gmail.com>
2021-07-04 00:45:31 -0500

    New branch Created and router stablished infrastructure

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./styles.scss";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+const HomePage = React.lazy(() => import("./pages/Home/Home.page"));
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/123">
+          <Suspense fallback="loading">
+            <HomePage />
+          </Suspense>
+        </Route>
+        <Redirect exact from="/*" to="/" />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
