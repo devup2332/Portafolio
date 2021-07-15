babef43dd4925433f08fb3cf874c9c035b64a0e4
Diego Raul <devup2332@gmail.com>
2021-07-15 14:34:26 -0500

    Added handlers of navbar

--- client/src/pages/Home/Components/Header/HeaderComponent.tsx
+++ client/src/pages/Home/Components/Header/HeaderComponent.tsx
@@ -6,7 +6,7 @@ const HeaderComponent = () => {
     //Handler for close Nav
     const closeNav = (e: Event) => {
         const btn = document.querySelector(".btn_menu_container");
-        if (!btn.contains(e.target as Node)) {
+        if (!btn?.contains(e.target as Node)) {
             document.querySelector(".header-nav")?.classList.remove("down");
             document.removeEventListener("click", closeNav);
         }
@@ -21,6 +21,7 @@ const HeaderComponent = () => {
     };
 
     useEffect(() => {
+        //Handler of active header when window's scroll is more than 200
         const handleActive = () => {
             if (document.documentElement.scrollTop > 200) {
                 document.querySelector(".header")?.classList.add("active");
@@ -28,13 +29,51 @@ const HeaderComponent = () => {
                 document.querySelector(".header")?.classList.remove("active");
             }
         };
+
+        //Handle windows scroll to set a active style to nav options
+        const listenNavScroll = () => {
+            const bannerH =
+                document.querySelector(".banner_cont")?.clientHeight;
+            const aboutH = document.querySelector(
+                ".aboutcomponent-container"
+            )?.clientHeight;
+            const options =
+                document.querySelector(".nav_menu_options")?.children;
+            const scroll = document.documentElement.scrollTop;
+
+            if (scroll < bannerH - 200) {
+                for (let i = 0; i < options.length; i++) {
+                    options[i].classList.remove("active");
+                }
+                options[0].classList.add("active");
+            }
+
+            if (scroll > bannerH - 200) {
+                for (let i = 0; i < options.length; i++) {
+                    options[i].classList.remove("active");
+                }
+                options[1].classList.add("active");
+            }
+            if (scroll > bannerH + aboutH - 200) {
+                for (let i = 0; i < options.length; i++) {
+                    options[i].classList.remove("active");
+                }
+                options[2].classList.add("active");
+            }
+        };
+
         document.addEventListener("scroll", handleActive);
 
+        document.addEventListener("scroll", listenNavScroll);
+
+        //Removing all linteners
         return () => {
             document.removeEventListener("click", closeNav);
             document.removeEventListener("scroll", handleActive);
+            document.removeEventListener("scroll", listenNavScroll);
         };
     }, []);
+
     return (
         <Fragment>
             <div className="header">
@@ -43,9 +82,36 @@ const HeaderComponent = () => {
 
                     <nav className="header-nav">
                         <ul className="nav_menu_options">
-                            <li className="option">Home</li>
-                            <li className="option">Projects</li>
-                            <li className="option">About</li>
+                            <li
+                                className="option"
+                                onClick={() => scrollTo(0, 0)}
+                            >
+                                Home
+                            </li>
+                            <li
+                                className="option"
+                                onClick={() =>
+                                    document
+                                        .querySelector(
+                                            ".aboutcomponent-container"
+                                        )
+                                        .scrollIntoView()
+                                }
+                            >
+                                About
+                            </li>
+                            <li
+                                className="option"
+                                onClick={() =>
+                                    document
+                                        .querySelector(
+                                            ".projects-component-container"
+                                        )
+                                        .scrollIntoView()
+                                }
+                            >
+                                Projects
+                            </li>
                             <NavLink to="/contact" className="option">
                                 Contact
                             </NavLink>
